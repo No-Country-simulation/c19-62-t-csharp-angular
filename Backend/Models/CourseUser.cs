48f009b49fd2656258b4495afc9f5f54bce46af8
Ignacio Conde <djkero@proton.me>
2024-07-09 16:02:17 -0300

    chore: Rename models to Models (Fix typo)

--- Backend/Models/CourseUser.cs
+++ Backend/Models/CourseUser.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Backend.models
+namespace Backend.Models
 {
     public class CourseUser
     {
