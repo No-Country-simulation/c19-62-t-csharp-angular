a5a71340d3cab4e84f8afdc36011f05a42e08b80
wanderlee <wanderleedev@gmail.com>
2024-07-09 10:28:55 -0500

    feat(frontend): :sparkles: basic structure of the form

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './shared/ui/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrls: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
--- frontend/src/app/shared/ui/login/login.component.ts
+++ frontend/src/app/shared/ui/login/login.component.ts
@@ -1,12 +1,22 @@
-import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  OnInit,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styles: `
     :host {
@@ -15,4 +25,28 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent { }
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(
+    private readonly formConstructor: FormBuilder,
+    private readonly destroy: DestroyRef
+  ) {
+    this.loginForm = this.formConstructor.nonNullable.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.loginForm.events
+      .pipe(takeUntilDestroyed(this.destroy))
+      .subscribe((data) => console.log(data));
+  }
+
+  public onSubmit(): void {
+    if (this.loginForm.invalid) return;
+
+    console.log(this.loginForm.value);
+  }
+}
