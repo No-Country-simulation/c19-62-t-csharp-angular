827f058966f6dcec9ff796e5af5d3df93b92f360
Ignacio Conde <djkero@proton.me>
2024-07-10 13:20:54 -0300

    chore: Remove unneeded paramater on User.cs

--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -13,7 +13,6 @@ namespace Backend.Models
 {
     public class User : IdentityUser
     {
-        [PersonalData]
         public string Name { get; set;} = String.Empty;
 
         public ICollection<CourseUser> CourseUsers { get; set; } = new List<CourseUser>();
