f34f0372353dfae4accfada013631fdfaa63aaa8
Wanderlee Max <116982723+WanderleeDev@users.noreply.github.com>
2024-07-11 13:16:36 -0500

    feat(frontend): :sparkles: routing and store implemented (#45)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import AUTH_ACTIONS from './core/store/auth/auth.actions';
+import { AppState } from './core/store/app.state';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template: `<router-outlet />`,
   styleUrls: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private readonly store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(AUTH_ACTIONS.auditToken());
+  }
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component'),
+  },
+  {
+    path: 'cms',
+    loadComponent: () => import('./pages/cms/cms.component'),
+  },
+];
