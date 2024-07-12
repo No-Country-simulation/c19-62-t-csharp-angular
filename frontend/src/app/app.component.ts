937f9d03d243e95e6211b5b980976177313232aa
wanderlee <wanderleedev@gmail.com>
2024-07-12 16:01:31 -0500

    implements layout in app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './shared/ui/login/login.component';
+import { Store } from '@ngrx/store';
+import AUTH_ACTIONS from './core/store/auth/auth.actions';
+import { AppState } from './core/store/app.state';
+import { BasicLayoutComponent } from './layouts/basic-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, BasicLayoutComponent],
+  template: `
+    <app-basic-layout>
+      <router-outlet />
+    </app-basic-layout>
+  `,
   styleUrls: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private readonly store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(AUTH_ACTIONS.auditToken());
+  }
+}
