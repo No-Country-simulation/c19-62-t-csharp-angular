afae8126088ac772c0e75ffd538c8b1d1765fc3a
Ignacio Conde <djkero@proton.me>
2024-07-18 22:07:34 +0000

    feature(Backend): Add user profile endpoints (#72)
    * refactor(backend): Move and improve User Register and Login endpoints
    
    * chore: Remove unneeded warning
    
    * refactor(backend): Rename Account service and controller to Role
    
    * chore(backend): Move User Dtos to a folder
    
    * chore(backend): Improve UserController
    
    * chore(backend): Add User DTO for login
    
    * feature(Backend): Embed user roles in JWT
    
    * feature(Backend): Improve embed user roles in JWT
    
    * chore(Backend): Add UserProfile DTO
    
    * feat(Backend): Added GetUser and UpdateUser endpoints
    
    * chore(Backend): UpdateProfile endpoint changed to HttpPut

--- Backend/Dtos/User/UserProfileDto.cs
+++ Backend/Dtos/User/UserProfileDto.cs
@@ -1,13 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Backend.Dtos
 {
-    public class UserGetDto
+    public class UserProfileDto
     {
-        public string Id { get; set; } = String.Empty;
         public string Email { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
         public string FirstName { get; set; } = String.Empty;
         public string LastName { get; set; } = String.Empty;
         public string PhoneNumber { get; set; } = String.Empty;
-        
-        public List<CourseUserDto> CourseUsers { get; set; } = new List<CourseUserDto>();
     }
 }
