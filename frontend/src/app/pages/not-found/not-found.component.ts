628f1a6b2e60412847dd8f4f485df6c958d10293
Alberto Gómez Juan <agomezjuan@hotmail.com>
2024-07-26 00:01:01 +0200

    feat(frontend): :sparkles: error 404 page (#83)
    * feat(frontend): :sparkles: error 404 page
    
    * fix(frontend): :art: change heading to h2

--- frontend/src/app/pages/not-found/not-found.component.ts
+++ frontend/src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BasicLayoutComponent } from 'app/layouts/basic-layout/basic-layout.component';
 import { BasicButtonComponent } from '../../shared/components/basic-button/basic-button.component';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [BasicLayoutComponent, BasicButtonComponent],
+  imports: [RouterLink, BasicLayoutComponent, BasicButtonComponent],
   templateUrl: './not-found.component.html',
   styles: `
     :host {
@@ -14,4 +15,9 @@ import { BasicButtonComponent } from '../../shared/components/basic-button/basic
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class NotFoundComponent {}
+export default class NotFoundComponent {
+  constructor(private readonly router: Router) {}
+  public navigateTo(path: string): void {
+    this.router.navigate([path]);
+  }
+}
